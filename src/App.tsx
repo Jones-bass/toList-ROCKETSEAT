3c3ff994d95da99da1b281ded56f09c1e475b61a
jones-bass <jonesbass.tb@gmail.com>
2022-06-12 13:10:08 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import { Header } from './Header/Header';
+import './global.css';
+
+export function App() {
+
+  return (
+      <div>
+        <Header/>
+
+        OLA DEV
+
+      </div>
+  )
+}
+
+
---
+++ src/Header/Header.tsx
@@ -0,0 +1,14 @@
+
+import styles from './Header.module.css';
+import logo from "../assets/logo.svg";
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="Logo Ignite" />
+      toList
+    </header>
+  )
+}
+
+
